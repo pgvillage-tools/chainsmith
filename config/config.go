1a48786f883574573009d4917586fc010879a97d
Gaby Schilders <gaby.schilders@gmail.com>
2025-03-03 20:12:16 +0100

    Added Viper support for reading the config file.

--- config/config.go
+++ config/config.go
@@ -1,31 +1,37 @@
-package config
+`package config
 
 import (
-	"os"
-	"gopkg.in/yaml.v3"
+	"github.com/spf13/viper"
+	"log"
 )
 
 type CertificateConfig struct {
-	CertPath   string `yaml:"cert_path"`
-	KeyPath    string `yaml:"key_path"`
-	CommonName string `yaml:"common_name"`
+	CertPath   string `mapstructure:"cert_path"`
+	KeyPath    string `mapstructure:"key_path"`
+	CommonName string `mapstructure:"common_name"`
 }
 
 type Config struct {
-	RootCAPath         string                        `yaml:"root_ca_path"`
-	IntermediateCAPath string                        `yaml:"intermediate_ca_path"`
-	Certificates       map[string]CertificateConfig `yaml:"certificates"`
+	RootCAPath         string                        `mapstructure:"root_ca_path"`
+	IntermediateCAPath string                        `mapstructure:"intermediate_ca_path"`
+	Certificates       map[string]CertificateConfig `mapstructure:"certificates"`
 }
 
-func LoadConfig(filename string) (*Config, error) {
-	data, err := os.ReadFile(filename)
-	if err != nil {
+func LoadConfig(configPath string) (*Config, error) {
+	viper.SetConfigFile(configPath)
+	viper.SetConfigType("yaml")
+
+	viper.AutomaticEnv() // Allow environment variable overrides
+
+	if err := viper.ReadInConfig(); err != nil {
 		return nil, err
 	}
 
 	var cfg Config
-	if err := yaml.Unmarshal(data, &cfg); err != nil {
+	if err := viper.Unmarshal(&cfg); err != nil {
 		return nil, err
 	}
+
+	log.Printf("Loaded configuration from %s", viper.ConfigFileUsed())
 	return &cfg, nil
 }
