2cc46ae9b91bdcd29ff501f8ab141d6bb30972ca
Sebastiaan Mannem <sebas@mannem.nl>
2025-09-16 01:59:23 +0200

    More unittests

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -8,8 +8,9 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Config", func() {
+var _ = Describe("Config", Ordered, func() {
 	Context("When loading config", func() {
+		var tmpDir string
 		var config1 = `
 chain:
   intermediates:
@@ -39,12 +40,15 @@ subject:
   CN: chainsmith
 tmpdir: /tmp/pgvillage/chainsmith/chain1/
 `
-		It("should return config as expected", func() {
-			tmpDir, err := os.MkdirTemp("", "configTest")
+		BeforeAll(func() {
+			var err error
+			tmpDir, err = os.MkdirTemp("", "configTest")
 			Expect(err).Error().NotTo(HaveOccurred())
-			defer func() {
-				Expect(os.RemoveAll(tmpDir)).Error().NotTo(HaveOccurred())
-			}()
+		})
+		AfterAll(func() {
+			Expect(os.RemoveAll(tmpDir)).Error().NotTo(HaveOccurred())
+		})
+		It("should return config as expected", func() {
 			confPath := path.Join(tmpDir, "config.yaml")
 			for _, config := range []string{config1, config2} {
 				Expect(os.WriteFile(confPath, []byte(config), 0600)).Error().NotTo(HaveOccurred())
@@ -58,6 +62,19 @@ tmpdir: /tmp/pgvillage/chainsmith/chain1/
 				Expect(chain.Intermediates).To(HaveLen(2))
 				Expect(chain.Root.Cert.Subject.CommonName).To(Equal("chainsmith"))
 			}
+		})
+		It("should raise error when file does not exist", func() {
+			confPath := path.Join(tmpDir, "does_not_exist.yaml")
+			_, err := LoadConfig(confPath)
+			Expect(err).Error().To(HaveOccurred())
+		})
+		It("should raise error when file does not parse as yaml", func() {
+			confPath := path.Join(tmpDir, "invalid.yaml")
+			Expect(
+				os.WriteFile(confPath, []byte("this is not valid yaml"), 0600),
+			).Error().NotTo(HaveOccurred())
+			_, err := LoadConfig(confPath)
+			Expect(err).Error().To(HaveOccurred())
 		})
 	})
 	Context("When converting a config to a Chain", func() {
